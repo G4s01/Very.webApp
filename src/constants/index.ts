1ce478ba772a68e39b879a708670a38a3e0c27db
_mitch.el <michele.gastaldi01@gmail.com>
2025-11-16 17:17:14 +0100

    Delete src directory

--- src/constants/index.ts
+++
@@ -1,4 +0,0 @@
-export const API_BASE = (process.env.API_BASE || 'https://apigw.verymobile.it/api').replace(/\/+$/, '');
-export const SESSION_WARN_SECONDS =
-  Number(process.env.NEXT_PUBLIC_SESSION_WARN_SECONDS || process.env.SESSION_WARN_SECONDS || 300) || 300;
-export { windHeaders } from './windHeaders';
--- src/lib/debug.ts
+++
@@ -1,6 +0,0 @@
-export function dlog(...args: any[]) {
-  if (process.env.DEBUG_AUTH === '1' || process.env.NEXT_PUBLIC_DEBUG === '1') {
-    // eslint-disable-next-line no-console
-    console.log(...args);
-  }
-}
